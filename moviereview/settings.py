c4b07e1cbd04cbe809a7f2d9bdd45cef08591a51
rattana neak <rattana.neak@gmail.com>
2019-02-11 18:55:52 -0800

    First view

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'review',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'moviereview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'moviereview',
+        'USER': 'rattananeak',
+        'PASSWORD': 'P@ssword1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
